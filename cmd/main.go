aa904ad77c865aecc3fe4703ed9ac95c3fc74552
AnthonyGOliveira <anthony.glivers@gmail.com>
2025-05-01 12:11:12 -0300

    feat: adding health check endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	port := 8000
+	log.Printf("Server is running on port %d\n", port)
+
+	http.HandleFunc("/health", healthCheckHandler)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
